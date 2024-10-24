941a9b5737f02a2f91020deb2ea3b779af168072
Sakshi Dappu <dappusakshi@gmail.com>
2024-10-24 19:41:03 +0530

    Dynamic Bg Image

--- src/WeatherPractice/InfoBox.jsx
+++ src/WeatherPractice/InfoBox.jsx
@@ -8,21 +8,36 @@ export default function InfoBox({ data, onClick }) {
         <>
           <div className="userInfo">
             {data.resolvedAddress} <br />
-          
             {new Date(data.days[0].datetimeEpoch * 1000).toLocaleString()}
-            
             <button className="newSearchBtn" onClick={onClick}>
               Check different location
             </button>
-          </div> 
+          </div>
 
           <div className="InfoCard">
-           <li>Temperature:&nbsp;&nbsp;{data.days[0].temp} &nbsp;°C </li>
-            <li> Description: &nbsp;&nbsp;{data.days[0].description}</li>
+            <li>Temperature:&nbsp;&nbsp;{data.days[0].temp} &nbsp;°C </li>
+            {data.days[0].description && (
+              <li> Description: &nbsp;&nbsp;{data.days[0].description}</li>
+            )}
+
             <li>Sky Looks:&nbsp;&nbsp; {data.days[0].icon}</li>
 
-            <li>Sunrise at: &nbsp;&nbsp;{`${parseInt(data.days[0].sunrise.split(":")[0]) % 12 || 12}:${data.days[0].sunrise.split(":")[1]} ${parseInt(data.days[0].sunrise.split(":")[0]) >= 12 ? 'PM' : 'AM'}`}</li>
-            <li>Sunset at:&nbsp; &nbsp; {`${parseInt(data.days[0].sunset.split(":")[0]) % 12 || 12}:${data.days[0].sunset.split(":")[1]} ${parseInt(data.days[0].sunset.split(":")[0]) >= 12 ? 'PM' : 'AM'}`}</li>
+            <li>
+              Sunrise at: &nbsp;&nbsp;
+              {`${parseInt(data.days[0].sunrise.split(":")[0]) % 12 || 12}:${
+                data.days[0].sunrise.split(":")[1]
+              } ${
+                parseInt(data.days[0].sunrise.split(":")[0]) >= 12 ? "PM" : "AM"
+              }`}
+            </li>
+            <li>
+              Sunset at:&nbsp; &nbsp;
+              {`${parseInt(data.days[0].sunset.split(":")[0]) % 12 || 12}:${
+                data.days[0].sunset.split(":")[1]
+              } ${
+                parseInt(data.days[0].sunset.split(":")[0]) >= 12 ? "PM" : "AM"
+              }`}
+            </li>
             <li>UV Index: &nbsp;&nbsp; {data.days[0].uvindex}</li>
             <li>Humidity:&nbsp; &nbsp;{data.days[0].humidity} &nbsp;°C</li>
           </div>
--- src/WeatherPractice/Weather.jsx
+++ src/WeatherPractice/Weather.jsx
@@ -4,11 +4,10 @@ import SearchBox from "./SearchBox";
 import InfoBox from "./InfoBox";
 import ErrorBox from "./ErrorBox";
 
-
 export default function Weather() {
   const [info, setInfo] = useState(null);
   const [error, setError] = useState(false);
-
+// const [bgImage, setBgImage] = useState("https://wallup.net/wp-content/uploads/2016/03/10/318375-nature-landscape-lake-mountain-forest-wildflowers-spring-pine_trees-path-Switzerland-HDR.jpg")
   const GetData = async (location, eventDate, eventTime) => {
     const API_Key = "YLZJQDHJTSGUDTYJVP3S4ATVZ";
     // const API_Key = process.env.API_Key;
@@ -35,40 +34,48 @@ export default function Weather() {
   const handleErrorSearch = () => {
     setError(false);
   };
+  const INIT_URL = "https://wallup.net/wp-content/uploads/2016/03/10/318375-nature-landscape-lake-mountain-forest-wildflowers-spring-pine_trees-path-Switzerland-HDR.jpg";
 
+const COLD_URL = "https://s4.scoopwhoop.com/anj/snow/50e4de95-1be0-4440-9efa-995d6eee52ee.jpg";
+const HOT_URL = "https://wallpapercave.com/wp/wp3098031.jpg";
+const WINDY_URL = "https://wallpapercave.com/wp/wp2108762.jpg";
+const RAIN_URL = "https://png.pngtree.com/background/20230616/original/pngtree-rain-falling-down-on-a-tree-leaves-picture-image_3626721.jpg";
 
+let bgImage = INIT_URL;
 
-  
-
-
-
-  // if (info) {
-  //   const humid = info.days[0].humidity;
-  //   console.log('Humidity level:', humid);
-  //   if (humid < 10) {
-  //     bgImage = COLD_URL;
-  //   } else if (humid >= 10 && humid < 50) {
-  //     bgImage = WINDY_URL;
-  //   } else if (humid >= 50 && humid < 75) {
-  //     bgImage = RAIN_URL;
-  //   } else {
-  //     bgImage = HOT_URL;
-  //   }
-  //   console.log('Background Image URL:', bgImage);
-  // }
+  if (info) {
+    const humid = info.days[0].humidity;
+    console.log('Humidity level:', humid);
+    if (humid < 10) {
+      bgImage = COLD_URL;
+    } else if (humid >= 10 && humid < 50) {
+    bgImage =   WINDY_URL;
+    } else if (humid >= 50 && humid < 75) {
+      bgImage = RAIN_URL;
+    } else {
+      bgImage = HOT_URL;
+    }
+    console.log('Background Image URL:', bgImage);
+  } 
 
   // Check based on conditions why the bg img are not appearing
   return (
+    // <div
+    //   className="newCont"
+    //   style={{
+    //     backgroundImage: `url(${bgImage})`,
+    //   }}
+    // >
     <div
       className="newCont"
-      // style={{
-      //   backgroundImage: `url(${bgImage})`,
-      // }}
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
     >
       {!info && error === false && (
         <SearchBox
           className="SearchBox"
-           onSearch={GetData}
+          onSearch={GetData}
           error={error}
           handleErrorSearch={handleErrorSearch}
         />
@@ -86,6 +93,3 @@ export default function Weather() {
     </div>
   );
 }
-
-
-
